dfcaa2c3ac962d27665ad0bf57499d1d19b8a9f9
Noura <nouraalmethin@gmail.com>
2022-08-03 18:37:46 +0300

    then

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var favouriteNumber = 2
+var name = "Noura"
+var hobby = "Reading"
+print("Hello my name Is \(name) and my favourite number Is \(favouriteNumber) and I love \(hobby) \n I was born In\(BirthOfDate)")
+
+var BirthOfDate = 2004
+
+var greeting = "Hello, playground"
